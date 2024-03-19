191ca5ccc5213f315eb4775555f14d6bae5d3a81
wmh001 <wang.maohua@foxmail.com>
2024-03-19 20:25:09 +0800

    变量

---
+++ 2.2.py
@@ -0,0 +1,21 @@
+# 2.2 变量
+
+# 变量存储值（与变量相关联的信息）
+# Python中，变量创建与首次赋值同时进行
+
+# 变量名 = 值<>
+# 给变量赋值或改变变量的值
+message = "Hello Python world"
+print(message)
+
+# 修改变量的值
+message = "Hello Python Crash world!"
+print(message)
+
+# 2.2.1 变量的命名和使用
+# 变量名命名原则：只包含字母、数字、下划线；不能以数字开头；不包含空格；不能用关键字和函数名；既简短又具有描述性；慎用I和O，以防误认为1和0
+
+# 2.2.2 使用变量时避免命名错误
+# 变量名拼写错误报错
+message = "Hello Python Crash Course reader!"
+print(mesage)
