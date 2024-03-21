a2291bbcef8a10cdb567bc603638f96ff43a0053
wmh001 <wang.maohua@foxmail.com>
2024-03-21 18:22:17 +0800

    一个简单示例

---
+++ 5.1.py
@@ -0,0 +1,13 @@
+# 5.1 一个简单示例
+cars = ["audi", "bmw", "subaru", "toyota"]
+for car in cars:
+    # if语句
+    # if 布尔表达式:
+    #     语句体1
+    # else:
+    #     语句体2
+    # 功能后文介绍
+    if car == "bmw":
+        print(car.upper())
+    else:
+        print(car.title())
