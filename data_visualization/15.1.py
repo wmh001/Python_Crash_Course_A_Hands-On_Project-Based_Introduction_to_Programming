e271a4d3d50015aef1a146822e2a8b46581c242f
wmh001 <wang.maohua@foxmail.com>
2024-03-29 21:13:39 +0800

    安装matplotlib

---
+++ data_visualization/15.1.py
@@ -0,0 +1,22 @@
+# 15.1 安装matplotlib
+
+# 15.1.1 在Linux系统中安装matplotlib
+# 系统自带Python版本：sudo apt-get install python3-matplotlib
+#     或sudo apt-get install python-matplotlib
+
+# 较新的Python版本：apt-get安装依赖库：python3.5-dev、python3.5-tk、tk-dev、libfreetype6-dev、g++
+#     pip安装matplotlib：pip install --user matplotlib
+
+# 15.1.2 在OS X系统中安装matplotlib
+# OS X的自带Python通常带有matplotlib
+# 如果没有：pip install --user matplotlib
+
+# 15.1.3 在windows系统中安装matplotlib
+# 访问https://pypi.python.org/pypi/matplotlib 寻找与Python版本对应的.whl格式matplotlib安装文件
+# 在安装文件的目录运行python -m pip install --user 文件名
+
+# 15.1.4 测试matplotlib
+# 启动Python环境，导入matplotlib不报错
+
+# 15.1.5 matplotlib画廊
+# 访问http://matplotlib.org 的示例画廊
